f057f15dc8517355172c44b1a0dd6f05be4f2b36
Sola-Ade <solaadefarasin@hotmail.com>
2024-06-21 15:06:39 +0100

    tribute page app

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,23 @@ import './App.css'
 
 function App() {
   return (
-    <main>
-      <h1>Travel information App</h1>
-      <h1>Travel information App</h1>
-    </main>
+    <>
+       <title>Adrian Monk</title>
+       <h1 id="title">Adrian Monk, the best homicide detective in the world!</h1>
+       <h2>Brief History</h2>
+       <p>
+       Monk is a renowned former homicide detective for the San Francisco Police Department. 
+       Monk has obsessive-compulsive disorder and multiple phobias, all of which intensified after the 
+       murder of his wife Trudy, resulting in his suspension from the police department. 
+       He works as a private police homicide consultant and undergoes therapy with the ultimate goal of overcoming 
+       his grief, taking control of his phobias and disorder, and being reinstated as a police detective. 
+       While Monk's personal challenges and compulsions often cause problems and frustration for both himself and those 
+       around him, his observational skills and keen attention to detail enable him to solve cases through 
+       unconventional means. His 312 phobias include germs, needles, birds, heights, dentists, milk, death, 
+       snakes, lightning, mushrooms, crowds and enclosed spaces.
+       </p>
+       <p>For more information, please visit <a id="tribute-link" href="https://en.wikipedia.org/wiki/Adrian_Monk" target="_blank">Adrian Monk on Wikipedia</a></p>
+    </>
   );
 }
 
